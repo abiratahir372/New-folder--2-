1801698a2c2a03a3ca7deb53e5588ce78bf63a32
Tahir Hussain <tahirhussaian0057@gmail.com>
2024-08-25 02:24:53 -0700

    testing

---
+++ file10.py
@@ -0,0 +1,54 @@
+# program 1
+#  Calculate your age based on the current year and your birth year.
+birth_year=int(input("enter your birthday year:"))
+current_year=int(input("enter current year:"))
+age=current_year-birth_year
+print("It your age=",(age)) 
+# program 2
+#  Write a program that calculates the area of a rectangle using length and width variables.
+length=int(input("enter the rectangle_length:"))
+widht=int(input("enter the width _length:"))
+area=length*widht
+print("It is the area of rectangle:",area)
+# program 3
+#  Write a program that calculates the area of a circle.
+radius=int(input("enter the radius"))
+pie=3.1416
+area_circule_radius=pie*radius**2
+print("area of circul",area_circule_radius)
+# program 4
+# Write a program that calculates the volume of the cube.
+cube_length=int(input("enter your cube length"))
+cube_width=int(input("enter your cube width"))
+cube_heigth=int(input("enter your cube heigth"))
+volume_cube=cube_heigth*cube_length*cube_width
+print("It is the volum of the cude",volume_cube)
+# program 5
+#  Convert a given number of seconds into minutes and seconds using variables.
+Time=int(input("enter time"))
+Time_2=Time*60
+print("Time in second",Time_2)
+# program 6
+# Write a program that calculates the percentage.
+marks=int(input("enter your obtain marks"))
+marks_2=int(input("enter totai marks"))
+percentage=marks/marks_2*100
+print("It is your percentage",percentage)
+# program 7
+#  Write a program that calculates the volume of a cylinder using the formula 
+radius=int(input("enter the adius"))
+heigth=int(input("enter the heigth"))
+pie=3.1416
+volume_cylinder=3.116*radius**2*heigth
+print("volume_cylinder",volume_cylinder)
+# program 8
+#  Create a program that converts a temperature from Fahrenheit to Celsius and vice versa using a variable.
+temp=int(input("enter the temperatur:"))
+temp_2=temp*9/5+32
+print("It is the tempratur in farrenhiet:"),temp_2
+temp_3=int(input("enter the temp again:"))
+temp_4=temp-32*5/9
+print("temprature in celcius",temp_4)
+
+# new code added
+print("Testing")
